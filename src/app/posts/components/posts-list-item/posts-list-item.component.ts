37d2dd6f7bbf244004044e6dd53b435e1ad79e8a
it-opes <programmer@it-opes.com>
2019-04-09 16:00:02 +0200

    feat(posts): enable OnPush strategy in change detection

--- src/app/posts/components/posts-list-item/posts-list-item.component.ts
+++ src/app/posts/components/posts-list-item/posts-list-item.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 
 @Component({
   selector: 'app-posts-list-item',
   templateUrl: './posts-list-item.component.html',
-  styleUrls: ['./posts-list-item.component.scss']
+  styleUrls: ['./posts-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostsListItemComponent implements OnInit {
 
   @Input()
   post: Post;
 
-  constructor() { }
+  constructor(private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.changeDetector.detectChanges();
+    }, 2000);
   }
 
   getAuthorLink() {
+    console.log('getAuthorLink');
+    // synchro: Model -> View
     return `/author/${this.post.author.id}`;
   }
 
